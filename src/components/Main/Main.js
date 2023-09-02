61a626f6f24f38e9a2baf5532e48db261e3c375f
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-09-02 14:18:37 +0800

    промо

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Footer from "../Footer/Footer";
+import Promo from "../Promo/Promo";
 
 function Main({ props }) {
   
     return (
       <main className="main">
+        <Promo></Promo>
         <Footer></Footer>
       </main>
     );

4f170e9fa8f1f59b470dbf20e6fd26a04634be93
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-09-02 13:36:12 +0800

    шрифты, футер и ссылки

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,23 @@
+function Footer() {
+  
+    return (
+      <footer className="footer">
+        <p className="footer__note footer__text">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+        <div className="footer__container">
+          <p className="footer__copyright footer__text">© 2020</p>
+          <ul className="footer__links">
+            <li>
+              <a className="footer__link footer__text link" href="https://practicum.yandex.ru/"
+              target="_blank" rel="noreferrer">Яндекс.Практикум</a>
+            </li>
+            <li>
+              <a className="footer__link footer__text link" href="https://github.com/"
+              target="_blank" rel="noreferrer">Github</a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../Footer/Footer";
+
+function Main({ props }) {
+  
+    return (
+      <main className="main">
+        <Footer></Footer>
+      </main>
+    );
+  }
+  
+  export default Main;
+  

64ce7ae261ee43c08582fc4779d3066138d88d96
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-09-11 14:46:13 +0800

    сверстать компонент Технологии

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,8 @@ import React from "react";
 import Footer from "../Footer/Footer";
 import Promo from "../Promo/Promo";
 import NavTab from "../NavTab/NavTab";
+import AboutProject from "../AboutProject/AboutProject";
+import Techs from "../Techs/Techs";
 
 function Main({ props }) {
   
@@ -9,6 +11,8 @@ function Main({ props }) {
       <main className="main">
         <Promo></Promo>
         <NavTab></NavTab>
+        <AboutProject></AboutProject>
+        <Techs></Techs>
         <Footer></Footer>
       </main>
     );

a2aea3c1af8a71974545b2d39f0154e03eb36930
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-09-18 18:13:34 +0800

    сверстать 404

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+function NotFound() {
+  return (
+    <div className="not-found">
+      <h1 className="not-found__title">404</h1>
+      <p className="not-found__text">Страница не найдена</p>
+      <Link to="/" className="not-found__link link">
+        Назад
+      </Link>
+    </div>
+  );
+}
+
+export default NotFound;
